a082918b73bc11e5fa353714d76e48085270524b
israellinno <115151223+israellinno@users.noreply.github.com>
2022-10-21 13:29:10 +0100

    Optimized for mobile and small screens

---
+++ starter/main.js
@@ -0,0 +1,19 @@
+// console.log('send me money')
+var hamburgerdiv = document.querySelector(".hamburger")
+var mobilelinks = document.querySelector(".mobile-link-holder")
+var mybackdrop = document.querySelector(".backdrop")
+
+function dosomething() {
+    hamburgerdiv.classList.toggle("showburger")
+    mobilelinks.classList.toggle("show-mobile-link-holder")
+    mybackdrop.classList.toggle("show-backdrop")
+}
+
+
+
+// var writeup = "i am okay"
+
+// function dosomething() {
+//     alert(writeup)
+
+// }
